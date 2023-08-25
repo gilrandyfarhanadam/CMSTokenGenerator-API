5a76612faca0a9042657be3e7e96fd0936f5235e
Gilrandy Farhan Adam <gilrandy.adam001@stud.fh-dortmund.de>
2023-08-25 20:28:46 +0200

    v0.1

--- CMSTokenGeneratorAPI/db/Check.cs
+++ CMSTokenGeneratorAPI/db/Check.cs
@@ -2,6 +2,7 @@ namespace DatabaseConnection
 {
     public interface ICheck
     {
+        //
         public bool IsExist(string obj);
         public bool IsValid(string obj);
         public bool IsMatch(string[] obj);
